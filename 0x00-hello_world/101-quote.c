d44c5b303cf6edafb46f8066a7f91b37ba3f7d44
ichi-gin <yhachami7@gmail.com>
2023-09-07 17:49:40 -0700

    fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
  * Description: print a quote using write function
  * 	ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return: 1 (error)
- */
+ * Return: 1 (error).
+*/
+
+
 int main(void)
 {
     char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
