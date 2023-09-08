148eeee7a89955c3c0c73e2e9b4efb1b75e137d1
ichi-gin <yhachami7@gmail.com>
2023-09-08 13:54:31 -0700

    fix 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 
-/**
- * main - Entry point of the program
- *
- * Description: Prints all possible combinations of single-digit numbers.
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
 int num1, num2;
 
-for (num1 = 0; num1 <= 9; num1++)
+for (num1 = 0; num1 <= 8; num1++)
 {
-for (num2 = 0; num2 <= 9; num2++)
+for (num2 = num1 + 1; num2 <= 9; num2++)
 {
 putchar(num1 + '0');
 putchar(num2 + '0');
 
-if (num1 != 9 || num2 != 9)
+if (num1 != 8 || num2 != 9)
 {
 putchar(',');
 putchar(' ');
