fb2e4e74b58866ae44322232af49ee4820632661
ichi-gin <yhachami7@gmail.com>
2023-09-07 18:33:39 -0700

    fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
+ *
+ * Description: Prints a message with proper grammar to the standard output.
  *
  * Return: Always 0 (Success)
  */
@@ -10,3 +12,4 @@ int main(void)
     printf("with proper grammar, but the outcome is a piece of art,\n");
     return (0);
 }
+
