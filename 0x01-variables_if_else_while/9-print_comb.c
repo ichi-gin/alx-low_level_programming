244191a12bd1bf9cfee61dbd9684afc783e815bf
ichi-gin <yhachami7@gmail.com>
2023-09-08 13:31:08 -0700

    fix for 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,21 +9,20 @@
  */
 int main(void)
 {
-int num1, num2;
+int num;
 
-for (num1 = 0; num1 <= 9; num1++)
+for (num = 0; num <= 9; num++)
 {
-for (num2 = 0; num2 <= 9; num2++)
-{
-putchar(num1 + '0');
-putchar(num2 + '0');
+putchar(num + '0');
 
-if (num1 != 9 || num2 != 9)
+if (num < 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
-}
+
+putchar('\n');
+
 return (0);
 }
