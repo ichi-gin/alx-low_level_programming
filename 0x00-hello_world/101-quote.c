ed673fe58b00ac723bab572b495e4b873e61e33f
ichi-gin <yhachami7@gmail.com>
2023-09-07 17:46:15 -0700

    fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,13 +3,14 @@
 /**
  * main - Entry point of the program
  *
- * Description: Prints a message to the standard error.
+ * Description: print a quote using write function
+ * 	ssize_t write(int fd, const void *buf, size_t count);
  *
- * Return: Always 1 (Exit with error)
+ * Return: 1 (error)
  */
 int main(void)
 {
-    char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(2, str, 59);
+    char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, quo, 59);
     return (1);
 }
