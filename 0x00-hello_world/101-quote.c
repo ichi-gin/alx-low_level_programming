8ebcdd5a968635e8f4a9b21b1c13f2e948b20826
ichi-gin <yhachami7@gmail.com>
2023-09-07 18:20:19 -0700

    fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
- *
- * Description: Prints a message to the standard error.
- *
- * Return: Always 1 (Exit with error)
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
     write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
