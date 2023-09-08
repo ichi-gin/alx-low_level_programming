fb9bb6afdb8e9b15a512e35d082f03a7ea97597b
ichi-gin <yhachami7@gmail.com>
2023-09-07 17:55:00 -0700

    fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Entry point of the program
  *
- * Description: print a quote using write function
- * 	ssize_t write(int fd, const void *buf, size_t count);
+ * Description: This program prints a quote using write function
  *
  * Return: 1 (error).
-*/
+ */
 
 
 int main(void)
