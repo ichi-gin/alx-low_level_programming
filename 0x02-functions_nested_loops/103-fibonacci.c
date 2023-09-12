8389097e0355dd584b67538add7da446fc973b64
ichi-gin <yhachami7@gmail.com>
2023-09-12 15:38:21 -0700

    fix for 14

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,18 +7,24 @@
  */
 int main(void)
 {
-long int a = 1, b = 2, temp, sum = 2;    
+long int a = 1, b = 2, temp, sum = 2;
+    
 while (1)
 {
-temp = a + b;        
+temp = a + b;
+        
 if (temp > 4000000)
-break;        
+break;
+        
 if (temp % 2 == 0)
-sum += temp;        
+sum += temp;
+        
 a = b;
 b = temp;
-}    
+}
+    
 printf("%ld\n", sum);
     
 return (0);
 }
+
