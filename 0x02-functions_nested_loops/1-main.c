10c4b0cf7c99a9eea2959d7f7f7daa835607f5d9
ichi-gin <yhachami7@gmail.com>
2023-09-12 12:33:09 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function calls the print_alphabet function
+ * and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
