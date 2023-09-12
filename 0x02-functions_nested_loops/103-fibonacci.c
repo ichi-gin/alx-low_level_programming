4c057fca16c5922610fbd2ddff02f0ffa5f0d2bf
ichi-gin <yhachami7@gmail.com>
2023-09-12 15:35:48 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+long int a = 1, b = 2, temp, sum = 2;    
+while (1)
+{
+temp = a + b;        
+if (temp > 4000000)
+break;        
+if (temp % 2 == 0)
+sum += temp;        
+a = b;
+b = temp;
+}    
+printf("%ld\n", sum);
+    
+return (0);
+}
