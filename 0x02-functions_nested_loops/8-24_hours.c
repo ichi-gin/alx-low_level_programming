ca32620351b87be0743520959af9b022d6ba7db6
ichi-gin <yhachami7@gmail.com>
2023-09-12 13:59:36 -0700

    task 8 fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,24 +5,18 @@
  */
 void jack_bauer(void)
 {
-int hour_tens, hour_ones, minute_tens, minute_ones;
+int hour, minute;
 
-for (hour_tens = 0; hour_tens <= 2; hour_tens++)
+for (hour = 0; hour < 24; hour++)
 {
-for (hour_ones = 0; hour_ones <= 9; hour_ones++)
+for (minute = 0; minute < 60; minute++)
 {
-for (minute_tens = 0; minute_tens <= 5; minute_tens++)
-{
-for (minute_ones = 0; minute_ones <= 9; minute_ones++)
-{
-_putchar(hour_tens + '0');
-_putchar(hour_ones + '0');
+_putchar((hour / 10) + '0');
+_putchar((hour % 10) + '0');
 _putchar(':');
-_putchar(minute_tens + '0');
-_putchar(minute_ones + '0');
+_putchar((minute / 10) + '0');
+_putchar((minute % 10) + '0');
 _putchar('\n');
-}
-}
 }
 }
 }
--- 0x02-functions_nested_loops/8-main.c
+++ 0x02-functions_nested_loops/8-main.c
@@ -1,22 +1,12 @@
 #include "main.h"
 
 /**
- * jack_bauer - Prints every minute of the day of Jack Bauer.
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-void jack_bauer(void)
+int main(void)
 {
-int hour, minute;
-
-for (hour = 0; hour < 24; hour++)
-{
-for (minute = 0; minute < 60; minute++)
-{
-_putchar((hour / 10) + '0');
-_putchar((hour % 10) + '0');
-_putchar(':');
-_putchar((minute / 10) + '0');
-_putchar((minute % 10) + '0');
-_putchar('\n');
-}
-}
+    jack_bauer();
+    return (0);
 }
