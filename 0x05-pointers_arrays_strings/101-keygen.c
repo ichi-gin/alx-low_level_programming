767c7fb0d36bc53d78c02d494122ba31e11c048e
ichi-gin <yhachami7@gmail.com>
2023-09-18 13:21:05 -0700

    hot fix

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,23 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 62 // 26 lowercase letters + 26 uppercase letters + 10 digits
+#define PASSWORD_LENGTH 62 /* 26 lowercase letters + 26 uppercase letters + 10 digits */
 
-int main() {
+int main(void)
+{
 char valid_chars[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 char password[101];
 int i;
 
-srand(time(NULL)); // Seed the random number generator with the current time
+srand(time(NULL)); /* Seed the random number generator with the current time */
 
-for (i = 0; i < 100; i++) {
+for (i = 0; i < 100; i++)
+{
 int random_index = rand() % PASSWORD_LENGTH;
 password[i] = valid_chars[random_index];
 }
 
-password[100] = '\0'; // Null-terminate the password
+password[100] = '\0'; /* Null-terminate the password */
 
 printf("%s\n", password);
 
-return 0;
+return (0);
 }
