6ce33cc833f50839d90a51ad3cf9da3e48c4410a
ichi-gin <yhachami7@gmail.com>
2023-09-21 18:48:50 -0700

    fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,40 +1,59 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * infinite_add - Adds two numbers stored as strings.
- * @n1: The first number as a string.
- * @n2: The second number as a string.
- * @r: The buffer to store the result.
- * @size_r: The size of the buffer.
- *
- * Return: If the result can be stored in r, return the pointer to r.
- *         Otherwise, return 0.
+ * infinite_add - add 2 strings.
+ * @n1: string1.
+ * @n2: string2.
+ * @r: buffer
+ * @size_r: buffer size
+ * Return: String with all letters in ROT13 base.
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, carry = 0;
-
-	for (i = 0; n1[i] != '\0'; i++)
-		;
-	for (j = 0; n2[j] != '\0'; j++)
-		;
-
-	if (i > size_r || j > size_r)
+	int a_len = 0, b_len = 0, carry = 0, a, b, sum, biggest;
+	
+	while (n1[a_len] != '\0')
+		a_len++;
+	while (n2[b_len] != '\0')
+		b_len++;
+	if (a_len > b_len)
+		biggest = a_len;
+	else
+		biggest = b_len;
+	if ((biggest + 1) >= size_r)
 		return (0);
-
-	r[size_r--] = '\0';
-	i--;
-	j--;
-
-	while (i >= 0 || j >= 0 || carry)
+	r[biggest + 1] = '\0';
+	
+	while (biggest >= 0)
 	{
-		int num1 = (i >= 0) ? n1[i--] - '0' : 0;
-		int num2 = (j >= 0) ? n2[j--] - '0' : 0;
-		int sum = num1 + num2 + carry;
-
-		carry = sum / 10;
-		r[size_r--] = (sum % 10) + '0';
+		a = (n1[a_len - 1] - '0');
+		b = (n1[b_len - 1] - '0');
+		if (a_len > 0 && b_len > 0)
+			sum = a + b + carry;
+		else if (a_len < 0 && b_len > 0)
+			sum = b + carry;
+		else if (a_len > 0 && b_len < 0)
+			sum = a + carry;
+		else
+			sum = carry;
+		
+		if (sum > 9)
+		{
+			carry = sum / 10;
+			sum = (sum % 10) + '0';
+		}
+		else
+		{
+			carry = 0;
+			sum = sum + '0';
+		}
+		r[biggest] = sum;
+		a_len--;
+		b_len--;
+		biggest--;
 	}
-
-	return (r + size_r + 1);
+	if (*(r) != 0)
+		return (r);
+	else
+		return (r + 1);
 }
