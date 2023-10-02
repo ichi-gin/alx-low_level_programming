8365597f8db2ccf73080f6fb53f2d781955c1923
ichi-gin <yhachami7@gmail.com>
2023-10-02 13:57:48 -0700

    fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
 *@argv: array of arguments
 *Return: 0
 */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+int main(int argc, char const *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 
 	return (0);
